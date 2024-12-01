07f16b4d0fcd5c4b254f16eb8e5e56de76a032a9
Michael Zinsmeister <m.zinsmeister@live.de>
2024-12-01 11:43:10 +0100

    day 1

---
+++ 01/src/main.rs
@@ -0,0 +1,33 @@
+use std::{collections::HashMap, fs::read_to_string, ops::DerefMut};
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+
+    for l in input.lines() {
+        let (left, right) = l.split_once("   ").unwrap();
+        list1.push(left.parse::<u32>().unwrap());
+        list2.push(right.parse::<u32>().unwrap());
+    }
+
+    list1.sort();
+    list2.sort();
+
+    let result: u32 = list1.iter().zip(list2.iter())
+        .map(|(&l, &r)| l.abs_diff(r))
+        .sum();
+
+    println!("Part 1 Result: {}", result);
+
+    let mut right_index = HashMap::<u32,u32>::new();
+    list2.iter().for_each(|e| *right_index.entry(*e).or_default() += 1);
+
+    let result2: u32 = list1.iter()
+        .map(|l| right_index.get(l).cloned().unwrap_or_default() * *l)
+        .sum();
+
+    println!("Part 2 Result: {}", result2);
+
+}
