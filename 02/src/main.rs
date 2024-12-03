b6397624508b628e912c40985512e6a6d65112aa
Michael Zinsmeister <m.zinsmeister@live.de>
2024-12-03 16:36:09 +0100

    add day 3

---
+++ 02/src/main.rs
@@ -0,0 +1,59 @@
+use aoclib::read_raw_input;
+
+fn check_report(r: &[i32]) -> bool {
+    let mut current = r[0];
+    if r[0] < r[1] {
+        // Increasing
+        for i in 1..r.len() {
+            let diff = r[i] - current;
+            if diff < 1 || diff > 3 {
+                return false;
+            }
+            current = r[i];
+        }
+    } else if r[0] > r[1] {
+        // Decreasing
+        for i in 1..r.len() {
+            let diff = current - r[i];
+            if diff < 1 || diff > 3 {
+                return false;
+            }
+            current = r[i];
+        }
+    } else {
+        return false;
+    }
+    true
+}
+
+fn main() {
+    let input = read_raw_input("input.txt");
+
+    let reports: Vec<Vec<i32>> = input.split("\n")
+            .filter(|line| !line.is_empty())
+            .map(|line| line.split(" ").map(|x| x.parse::<i32>().unwrap()).collect())
+            .collect();
+
+    let result1 = reports.iter().filter(|r| check_report(r)).count();
+    let mut result2 = 0;
+
+    // Brute force
+    // There's an O(n) solution to be had here but i spent too much time trying to make it work
+    // General Idea: Have a "joker" that you set if you skip a number. First number needs special handling
+
+    'outer:
+    for r in reports.iter() {
+        for remove in 0..r.len() {
+            let mut r = r.clone();
+            r.remove(remove);
+            if check_report(&r) {
+                result2 += 1;
+                continue 'outer;
+            }
+        }
+    }
+
+    
+    println!("Result 1: {}", result1);
+    println!("Result 2: {}", result2);
+}
---
+++ 03/src/main.rs
@@ -0,0 +1,26 @@
+
+
+fn main() {
+    let raw_input = aoclib::read_raw_input("input.txt");
+
+    let regex = regex::Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+
+    let result = regex.captures_iter(&raw_input).into_iter().map(|c| c[1].parse::<i32>().unwrap() * c[2].parse::<i32>().unwrap()).sum::<i32>();
+
+    println!("Result1: {}", result);
+
+    let regex = regex::Regex::new(r"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\))").unwrap();
+
+    let mut state = true;
+    let mut result = 0;
+
+    regex.captures_iter(&raw_input).into_iter().for_each(|c| {
+        match &c[1] {
+            "do()" => state = true,
+            "don't()" => state = false,
+            _ => if state { result += c[2].parse::<i32>().unwrap() * c[3].parse::<i32>().unwrap() }
+        }
+    });
+
+    println!("Result2: {}", result);
+}
